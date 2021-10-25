fede4b37fd1fb6de49fd960dc96466e1b2fd25cf
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-10-25 22:36:33 +0300

    Add a notification to admins when the bot starts working

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from aiogram import executor
 from loader import dp
 
 from functionality.set_bot_commands import set_default_commands
+from functionality.notify_admins import notify_admins_at_bot_start
 
 
 async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
+    await notify_admins_at_bot_start(dispatcher)
 
 
 if __name__ == '__main__':

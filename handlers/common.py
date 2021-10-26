bcc75c61d9fbfbf09092af695009f158d74cae3d
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-10-26 14:28:52 +0300

    Set up a welcome message when the bot starts working

--- handlers/common.py
+++ handlers/common.py
@@ -1,10 +1,18 @@
 from aiogram import types, Dispatcher
+from aiogram.dispatcher.filters.builtin import CommandStart
 
 from keyboards.default.main_menu import main_menu_keyboard
 
 
 async def command_start(message: types.Message):
-    pass
+    channel_name = "@channel_name"
+    start_welcome_message = ("Welcome!",
+                             "This bot helps to hide your messages in images.",
+                             f"Also recommend subscribing to {channel_name} to keep up to date with the latest changes.")
+    start_useful_commands_message = ("Useful commands to get started:",
+                                     "/language - change language\n/help - detailed instructions")
+    await message.answer("\n\n".join(start_welcome_message))
+    await message.answer("\n\n".join(start_useful_commands_message), reply_markup=main_menu_keyboard)
 
 
 async def command_menu(message: types.Message):
@@ -28,4 +36,5 @@ async def button_support(message: types.Message):
 
 
 def register_handlers_common(dp: Dispatcher):
+    dp.register_message_handler(command_start, CommandStart())
     dp.register_message_handler(command_menu, commands="menu")

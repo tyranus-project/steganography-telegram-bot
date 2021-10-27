2dbb37811e2c6c1d6954c83234c09b3711300b92
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-10-27 11:35:40 +0300

    Add Encryption keyboard

---
+++ keyboards/default/cryption.py
@@ -0,0 +1,11 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+encryption_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Start encryption again"),
+            KeyboardButton(text="Cancel")
+        ]
+    ],
+    resize_keyboard=True
+)

b91a13bd3765c3125ec493a228dd3b00b9c6602f
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-11-03 12:25:28 +0300

    Refactor common handlers

--- handlers/common.py
+++ handlers/common.py
@@ -5,32 +5,37 @@ from aiogram.dispatcher.filters.builtin import CommandHelp
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from functionality.backend_processes import reset_user_data
+
 from keyboards.default.main_menu import main_menu_keyboard
 
 
-async def command_start(message: types.Message, state: FSMContext):
+async def cmd_start(message: types.Message, state: FSMContext):
     await reset_user_data(message, state)
-    channel_name = "@channel_name"
-    start_welcome_message = ("Welcome!",
-                             "This bot helps to hide your messages in images.",
-                             f"Also recommend subscribing to {channel_name} to keep up to date with the latest changes.")
-    start_useful_commands_message = ("Useful commands to get started:",
-                                     "/language - change language\n/help - detailed instructions")
-    await message.answer("\n\n".join(start_welcome_message))
-    await message.answer("\n\n".join(start_useful_commands_message), reply_markup=main_menu_keyboard)
+    await message.answer(
+        "Welcome!\n\n"
+        "This bot helps to hide your secret messages inside images!\n\n"
+        "Also recommend subscribing to @durov to keep up to date with the latest changes."
+    )
+    await message.answer(
+        "Useful commands to get started:\n\n"
+        "/help - detailed instructions",
+        reply_markup=main_menu_keyboard
+    )
 
 
-async def command_menu(message: types.Message, state: FSMContext):
+async def cmd_menu(message: types.Message, state: FSMContext):
     await reset_user_data(message, state)
-    await message.answer("Main menu", reply_markup=main_menu_keyboard)
+    await message.answer(
+        "Main menu",
+        reply_markup=main_menu_keyboard
+    )
 
 
-async def command_help(message: types.Message):
-    instruction_url = "instruction_URL"
-    faq_url = "FAQ_URL"
-    help_message = (f"Instructions: {instruction_url}",
-                    f"FAQ: {faq_url}")
-    await message.answer("\n\n".join(help_message))
+async def cmd_help(message: types.Message):
+    await message.answer(
+        "Instructions: https://telegram.org/faq\n\n"
+        "FAQ: https://telegram.org/faq"
+    )
 
 
 async def cancel(message: types.Message, state: FSMContext):
@@ -43,7 +48,7 @@ async def cancel(message: types.Message, state: FSMContext):
 
 
 def register_handlers_common(dp: Dispatcher):
-    dp.register_message_handler(command_start, CommandStart(), state="*")
-    dp.register_message_handler(command_help, CommandHelp(), state="*")
-    dp.register_message_handler(command_menu, commands="menu", state="*")
-    dp.register_message_handler(cancel, Text(equals="Cancel", ignore_case=True), state="*")
+    dp.register_message_handler(cmd_start, CommandStart(), state="*")
+    dp.register_message_handler(cmd_help, CommandHelp(), state="*")
+    dp.register_message_handler(cmd_menu, commands="menu", state="*")
+    dp.register_message_handler(cancel, Text(equals="cancel", ignore_case=True), state="*")

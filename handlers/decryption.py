f157752201383216f0a31fe7439849eb2144e3bc
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-11-03 12:42:33 +0300

    Rename states

--- handlers/decryption.py
+++ handlers/decryption.py
@@ -1,30 +1,31 @@
 from aiogram import types, Dispatcher
-from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 
-from states.crypting import Decrypting
+import config
 
-from functionality.backend_processes import save_user_file_as_image
-from functionality.backend_processes import reset_user_data
 from functionality.backend_processes import decrypt_stego_image
+from functionality.backend_processes import reset_user_data
+from functionality.backend_processes import save_user_file_as_image
 
 from keyboards.default.cryption import decryption_keyboard
 from keyboards.default.main_menu import main_menu_keyboard
 
-import config
+from states.crypting import Decrypt
+
 
 async def decrypting_start(message: types.Message, state: FSMContext):
     await reset_user_data(message, state)
     await message.answer("Send image with encrypted message", reply_markup=decryption_keyboard)
     await message.answer("IMPORTANT!\nImage must be sent as a file")
-    await Decrypting.waiting_for_image.set()
+    await Decrypt.waiting_for_stego_image.set()
 
 
 async def encrypted_image_entering(message: types.Message, state: FSMContext):
     await message.answer("Enter the password to find out the message hidden in the sent image")
     user_encrypted_image = await save_user_file_as_image(message, 'png')
     await state.update_data(stego_image=user_encrypted_image)
-    await Decrypting.next()
+    await Decrypt.next()
 
 
 async def decryption_password_entering_decrypting_end(message: types.Message, state: FSMContext):
@@ -42,5 +43,5 @@ async def decryption_password_entering_decrypting_end(message: types.Message, st
 def register_handlers_decryption(dp: Dispatcher):
     dp.register_message_handler(decrypting_start, Text(equals="Decrypt", ignore_case=True))
     dp.register_message_handler(decrypting_start, Text(equals="Start decryption again"), state="*")
-    dp.register_message_handler(encrypted_image_entering, content_types=['document'], state=Decrypting.waiting_for_image)
-    dp.register_message_handler(decryption_password_entering_decrypting_end, state=Decrypting.waiting_for_password)
+    dp.register_message_handler(encrypted_image_entering, content_types=['document'], state=Decrypt.waiting_for_stego_image)
+    dp.register_message_handler(decryption_password_entering_decrypting_end, state=Decrypt.waiting_for_decryption_key)
--- states/crypting.py
+++ states/crypting.py
@@ -1,12 +1,12 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
-class Encrypting(StatesGroup):
-    waiting_for_message = State()
-    waiting_for_image = State()
-    waiting_for_password = State()
+class Encrypt(StatesGroup):
+    waiting_for_secret_message = State()
+    waiting_for_image_container = State()
+    waiting_for_encryption_key = State()
 
 
-class Decrypting(StatesGroup):
-    waiting_for_image = State()
-    waiting_for_password = State()
+class Decrypt(StatesGroup):
+    waiting_for_stego_image = State()
+    waiting_for_decryption_key = State()

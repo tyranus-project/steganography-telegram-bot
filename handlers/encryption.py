78fb00de7c520f45dc1198e87a7d99e0b55681a7
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-11-03 13:52:52 +0300

    Refactor encryption handlers

--- handlers/encryption.py
+++ handlers/encryption.py
@@ -14,40 +14,46 @@ from keyboards.default.main_menu import main_menu_keyboard
 from states.crypting import Encrypt
 
 
-async def encrypting_start(message: types.Message, state: FSMContext):
+async def start_encrypt(message: types.Message, state: FSMContext):
     await reset_user_data(message, state)
-    await message.answer("Enter the text you want to encrypt", reply_markup=encryption_keyboard)
+    await message.answer(
+        "Enter the secret text you want to encrypt",
+        reply_markup=encryption_keyboard
+    )
     await Encrypt.waiting_for_secret_message.set()
 
 
-async def secret_message_entering(message: types.Message, state: FSMContext):
+async def enter_secret_message(message: types.Message, state: FSMContext):
     await state.update_data(secret_message=message.text)
-    await message.answer("Send an image in which the text will be encrypted")
+    await message.answer("Send an image in which the secret text will be hidden and encrypted")
     await Encrypt.next()
 
 
-async def original_image_entering(message: types.Message, state: FSMContext):
-    await message.answer("Enter the password with which you encrypt your text, and you can also decrypt later")
-    original_image_as_document = await save_user_file_as_image(message, 'jpg')
-    await state.update_data(image_container=original_image_as_document)
+async def enter_image_container(message: types.Message, state: FSMContext):
+    await message.answer("Enter the password to encrypt your secret text now and decrypt later")
+    user_image = await save_user_file_as_image(message, 'jpg')
+    await state.update_data(image_container=user_image)
     await Encrypt.next()
 
 
-async def encryption_password_entering_encrypting_end(message: types.Message, state: FSMContext):
+async def enter_encryption_key(message: types.Message, state: FSMContext):
     if config.USE_BOT_SALT:
         await state.update_data(encryption_key=message.text + config.BOT_SALT)
     else:
         await state.update_data(encryption_key=message.text)
     user_data = await state.get_data()
     encrypted_stego_container = create_encrypted_stego_image(**user_data)
-    await message.answer_document(types.InputFile(encrypted_stego_container), reply_markup=main_menu_keyboard)
-    await message.answer(f"Your text is encrypted in the file (image) above")
+    await message.answer_document(types.InputFile(encrypted_stego_container))
+    await message.answer(
+        "Your text is hidden and encrypted in the file (image) above",
+        reply_markup=main_menu_keyboard
+    )
     await reset_user_data(message, state)
 
 
 def register_handlers_encryption(dp: Dispatcher):
-    dp.register_message_handler(encrypting_start, Text(equals="Encrypt", ignore_case=True))
-    dp.register_message_handler(encrypting_start, Text(equals="Start encryption again"), state="*")
-    dp.register_message_handler(secret_message_entering, state=Encrypt.waiting_for_secret_message)
-    dp.register_message_handler(original_image_entering, content_types=['document'], state=Encrypt.waiting_for_image_container)
-    dp.register_message_handler(encryption_password_entering_encrypting_end, state=Encrypt.waiting_for_encryption_key)
+    dp.register_message_handler(start_encrypt, Text(equals="Encrypt", ignore_case=True))
+    dp.register_message_handler(start_encrypt, Text(equals="Start encryption again"), state="*")
+    dp.register_message_handler(enter_secret_message, state=Encrypt.waiting_for_secret_message)
+    dp.register_message_handler(enter_image_container, content_types=['document'], state=Encrypt.waiting_for_image_container)
+    dp.register_message_handler(enter_encryption_key, state=Encrypt.waiting_for_encryption_key)

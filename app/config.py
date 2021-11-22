bc195dce9b690c92be994d94384a4c29c4b2d454
neuromeow <92692649+neuromeow@users.noreply.github.com>
2021-11-22 14:41:53 +0300

    Add and set up environment variable for throttling rate limit

--- app/config.py
+++ app/config.py
@@ -7,3 +7,4 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")
 
 SKIP_UPDATES = env.bool("SKIP_UPDATES", default=False)
+THROTTLING_RATE_LIMIT = env.float("THROTTLING_RATE_LIMIT", default=0.1)
--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -2,9 +2,11 @@ from aiogram import Dispatcher
 
 from loguru import logger
 
+from app.config import THROTTLING_RATE_LIMIT
+
 from .throttling import ThrottlingMiddleware
 
 
 def setup_middlewares(dp: Dispatcher):
     logger.info("Configure middlewares...")
-    dp.setup_middleware(ThrottlingMiddleware(.5))
+    dp.setup_middleware(ThrottlingMiddleware(THROTTLING_RATE_LIMIT))
